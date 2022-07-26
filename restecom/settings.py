4f112f5264790915f7b42b5f85bab2245662c495
mhdshahidak <mhdshd.ak@gmail.com>
2022-07-26 17:03:23 +0530

    third push

--- restecom/settings.py
+++ restecom/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+    
     'ecom',
     'common',
 ]
@@ -118,6 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 STATIC_URL = "/static/"
 STATIC_FILE_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = ((BASE_DIR / "static"),)
